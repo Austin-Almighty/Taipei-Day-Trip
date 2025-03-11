889198987998cb9fc252b0f70ae343f09bee7f80
tim479863 <131895709+tim479863@users.noreply.github.com>
2025-03-11 02:13:10 -0700

    Complete the first API

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from fastapi import *
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
+import mysql.connector
+from typing import Annotated
+from starlette.middleware.sessions import SessionMiddleware
+from config import config
+
+cnx = mysql.connector.connect(**config)
+
 app=FastAPI()
 
 # Static Pages (Never Modify Code in this Block)
@@ -15,3 +22,42 @@ async def booking(request: Request):
 @app.get("/thankyou", include_in_schema=False)
 async def thankyou(request: Request):
 	return FileResponse("./static/thankyou.html", media_type="text/html")
+
+@app.get('/api/attractions')
+async def find_attractions(request: Request, page: int = Query(), keyword: str = Query(default=None)) :
+	if keyword:
+		cursor = cnx.cursor()
+		cursor.execute("select * from attractions where mrt = %s and name like %s;", (keyword,f"%{keyword}%"))
+		results = cursor.fetchall()
+	else:
+		cursor = cnx.cursor()
+		cursor.execute("select * from attractions")
+		results = cursor.fetchall()
+	
+	try:
+		start = page*12
+		end = (page+1)*12
+		spliced_results = results[start:end]
+		next_page = page + 1 if end < len(results) else None
+		data_list = []
+
+		for item in spliced_results:
+			data = {}
+			data['id'] = item[0]
+			data['name'] = item[1]
+			data['category'] = item[2]
+			data['description'] = item[3]
+			data['address'] = item[4]
+			data['transport'] = item[5]
+			data['mrt'] = item[6]
+			data['lat'] = item[7]
+			data['lng'] = item[8]
+			data['images'] = item[9]
+			data_list.append(data)
+			
+		response = {"nextPage":next_page, "data":data_list}
+		return JSONResponse(response, status_code=200)
+		
+	except Exception as e:
+		return JSONResponse({"error": True, "message": str(e)}, status_code=500)
+	
---
+++ data.py
@@ -0,0 +1,70 @@
+# custom functions that format the JSON file and save its data to MYSQL database
+
+import json
+import mysql.connector
+import re
+from config import config
+
+cnx = mysql.connector.connect(**config)
+
+def read_json(file):
+    with open(file, mode='r') as f:
+        data = json.load(f)
+    return data["result"]["results"]
+
+def image_urls(string):
+    pattern = r"(https.+?(?:\.jpg|\.JPG|\.png|\.PNG))"
+    image_urls = re.findall(pattern, string)
+    return image_urls
+
+def format_for_sql(file):
+    attractions = read_json(file)
+    tem = []
+    for location in attractions:
+        formatted_data = {}
+        formatted_data["id"] = location["_id"]
+        formatted_data["name"] = location["name"]
+        formatted_data["category"] = location["CAT"]
+        formatted_data["description"] = location["description"]
+        formatted_data["address"] = location["address"]
+        formatted_data["transport"] = location["direction"]
+        formatted_data["mrt"] = location["MRT"]
+        formatted_data["lat"] = location["latitude"]
+        formatted_data["lng"] = location["longitude"]
+        formatted_data["images"] = image_urls(location["file"])
+        tem.append(formatted_data)
+    return tem
+
+def write_to_db(file):
+    data = format_for_sql(file)
+    cursor = cnx.cursor()
+
+    query = """
+        INSERT INTO attractions(
+            id, name, category, description, address, transport, mrt, lat, lng, images, api
+        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+    """
+    
+    for location in data:
+        row_id = location.get('id')
+        name = location.get('name')
+        category = location.get('category')
+        description = location.get('description')
+        address = location.get('address')
+        transport = location.get('transport')
+        mrt = location.get('mrt')
+        lat = location.get('lat')
+        lng = location.get('lng')
+        images = json.dumps(location.get('images'))
+        api = json.dumps(location)
+        
+        values = (row_id, name, category, description, address, transport, mrt, lat, lng, images, api)
+        cursor.execute(query, values)
+
+    cnx.commit()
+
+file = "data/taipei-attractions.json"
+write_to_db(file)
+
+
+
